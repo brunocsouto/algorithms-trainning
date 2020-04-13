4a3acadc3161eef9918fc1e8f2fd5d331d7efb58
Bruno Souto <bruno.csouto@gmail.com>
2020-04-13 10:43:17 -0300

    Created Person class

---
+++ Person.java
@@ -0,0 +1,20 @@
+public class Person{
+	String name;
+	String personality;
+	boolean isSitting;
+	Robot robotOwned;
+
+	Person(String name, String personality, boolean isSitting){
+		this.name = name;
+		this.personality = personality;
+		this.isSitting = isSitting;
+	}
+
+	void sitDown(){
+		this.isSitting = true;
+	}
+
+	void standUp(){
+		this.isSitting = false;
+	}
+}
