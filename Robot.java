3e8437f1ec2c79260c29af5b4a77dc6710a2027a
Bruno Souto <bruno.csouto@gmail.com>
2020-04-13 08:17:39 -0300

    Created first sample class

---
+++ Robot.java
@@ -0,0 +1,21 @@
+public class Robot{
+	String name;
+	String color;
+	int weight;
+
+	Robot(String name, String color, int weight){
+		this.name = name;
+		this.color = color;
+		this.weight = weight;
+	}
+
+	void introduceSelf(){
+		System.out.println("Hi, my name is " + this.name);
+	}
+
+	public static void main(String[] args){
+		Robot r1 = new Robot("Tom", "gray", 50);
+
+		r1.introduceSelf();
+	}
+}
